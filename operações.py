26a7bb9efd48b38cdbd8da381efab05ebd900d89
Priscila-rm <rodriguesmontep20@gmail.com>
2025-05-07 17:34:11 -0300

    Operações em pyton.
    Aula de operações aritmética e relacionadas.

---
+++ operações.py
@@ -0,0 +1,31 @@
+a = 4
+b = 10
+c = 5.0
+d = 1
+f = 5
+
+print (a==c) 
+print (a<b)
+print (d>b)
+print (c!=f)
+print (a==b)
+print (c<d)
+print (b>a)
+print (c>=f)
+print (f>=c)
+print (c<=c)
+print (c<=f)
+
+s= "The most old"
+
+print(s[1:3]) # intervalo ente 1 e 3
+print(s[-3:-1]) #ol
+print(s[-3:]) #old 
+print(s[-3:0]) #
+
+p = "priscila"
+
+print(p.startswith("prisci"))
+print(len(p))
+print("cila" in (p))
+
