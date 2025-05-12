3bd91e4e3ca33b235f39ab0bbf6fd2cfb2b5c235
Priscila-rm <rodriguesmontep20@gmail.com>
2025-05-12 17:52:05 -0300

    Calculo de porcentagem

---
+++ calculo_de_porcentagem.py
@@ -0,0 +1,8 @@
+# calculo de porcentagem de numero.
+
+#Pedindo pro usuario digitar o valor total 
+valor_total = float(input('Digite um valor total:'))
+
+#pedindo pro usuario digitar em porcentagem
+porcentagem = float(input('valor em porcentagem:'))
+
