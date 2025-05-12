2f12435ee5790122063de76c8359e4666d07fe23
Priscila-rm <rodriguesmontep20@gmail.com>
2025-05-12 17:21:02 -0300

    criacao de operadores booleanos
    operadores booleanos

---
+++ main.py
@@ -0,0 +1,30 @@
+# operadores booleanos verdadeiro (true) ou falso (false)
+
+a = 3 > 4
+#print(a)
+# sinal de diferente (!=)
+b = 7 != 7
+#print (b)
+
+#comparação composto
+composto = 9 == 10 - 1
+#print(composto)
+
+composto2 = 5 + 4 <= 11 - 2
+#print(composto2)
+
+
+#ordem aritmetica
+#exemplo de divisâo com saida float
+divisao_saida_float = 4 / 2
+#print(f'Esse é o resultado da divisao com / saída float {divisao_saida_float}')
+
+
+divisao_saida_int = 4 // 2
+#print('Esse é o resultado da divisao com saída inteiro, divisao_saida_int')
+
+divisao_resto = 4 % 2
+#print(divisao_resto)
+
+resultado = 3 + 4 * 2
+#print(f'Esse é o resultado do calculo de 3 + 4 * 2  é : { resultado}')
